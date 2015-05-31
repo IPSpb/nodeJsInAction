0177eed2db7b44ee9fb766b806538ec8ddb8a8ca
IPopov <ip.webmaster.spb@gmail.com>
2015-05-31 16:51:24 +0300

    Hello world

---
+++ 1.Hello World/hello_world.js
@@ -0,0 +1,29 @@
+/**
+ * Http сервер Hello World
+ */
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
+
+
+/**
+ *
+ * Тоже самое но более явно.
+ */
+/*var http =require('http'),
+    server = http.createServer();
+
+server.on('request', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+server.listen(3000);
+
+console.log('Server running at http://localhost:3000/');*/
+
